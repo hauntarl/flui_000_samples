b5c74e30035b181648182bb920c2f337f1e807c3
hauntarl <sameer.mungole@gmail.com>
2020-08-16 01:35:20 +0530

    minor tweaks

--- lib/google_io_filter/google_io_filter.dart
+++ lib/google_io_filter/google_io_filter.dart
@@ -8,13 +8,42 @@ class GoogleIOFilter extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.white,
         body: Center(
-          child: FilterOption(
-            duration: Duration(milliseconds: 300),
-            filterText: 'filter option',
-            filterColor: Colors.deepPurple[300],
-            backgroundColor: Theme.of(context).canvasColor,
-            onTap: (val) => print(val),
+          child: Wrap(
+            alignment: WrapAlignment.start,
+            spacing: 5,
+            runSpacing: 10,
+            children: [
+              FilterOption(
+                duration: Duration(milliseconds: 300),
+                filterText: 'filter option',
+                unselectedFilterTextColor: Colors.black54,
+                filterColor: Colors.deepPurple[300],
+                onTap: (val) => print(val),
+              ),
+              FilterOption(
+                duration: Duration(milliseconds: 600),
+                filterText: 'flutter',
+                unselectedFilterTextColor: Colors.indigo[300],
+                filterColor: Colors.indigo[300],
+                onTap: (val) => print(val),
+              ),
+              FilterOption(
+                duration: Duration(milliseconds: 900),
+                filterText: 'Go',
+                unselectedFilterTextColor: Colors.teal[300],
+                filterColor: Colors.teal[300],
+                onTap: (val) => print(val),
+              ),
+              FilterOption(
+                duration: Duration(milliseconds: 1200),
+                filterText: 'Python',
+                unselectedFilterTextColor: Colors.blue[300],
+                filterColor: Colors.amber[300],
+                onTap: (val) => print(val),
+              ),
+            ],
           ),
         ),
       ),
