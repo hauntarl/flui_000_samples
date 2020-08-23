8b4505c62834841e60e12c6b59d19198c0547988
hauntarl <sameer.mungole@gmail.com>
2020-08-23 18:52:56 +0530

    removed collapsible sidebar from samples

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,7 @@
 import 'package:flutter/material.dart';
 
-// import './google_io_filter/google_io_filter.dart';
+import './google_io_filter/google_io_filter.dart';
 // import './custom_painter_demo/custom_painter_demo.dart';
-import './collapsible_side_bar/side_bar_page.dart';
 
-// void main() => runApp(GoogleIOFilter());
+void main() => runApp(GoogleIOFilter());
 // void main() => runApp(CustomPainterDemo());
-void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: SideBarPage(),
-      ),
-    ),
-  );
-}
